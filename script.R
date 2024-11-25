7d798bd301a2ac8d5ccd8fbc807a29c0063417d6
eolybq <jez.vasek@gmail.com>
2024-11-25 21:14:37 +0100

    maly zbytky

--- script.R
+++ script.R
@@ -5,7 +5,7 @@ library(tseries)
 library(urca)
 library(vars)
 
-# načtení dat
+#načtení dat
 hcpi <- read_excel("data/cz/hcpi.xlsx", n_max = 297, skip = 6)
 unemp <- read_excel("data/cz/unemp.xlsx", range = "A9:HU101")
 b_sheet <- read_delim("data/cz/rozvaha_cnb.csv", delim = ";")
@@ -14,31 +14,37 @@ fg_uncomplete <- read_excel("data/cz/fg.xlsx")
 ie_p <- read_excel("data/cz/CZ_p_m.xlsx")
 ie_h <- read_excel("data/cz/CZ_h_m.xlsx")
 
-#doplnění implicitních chybějících hodnot ve FG
+#doplnění implicitních chybějících hodnot ve FGa rozdeleni na uvoneni a zprisneni
 fg_uncomplete$time <- format(fg_uncomplete$time, "%Y-%m")
 fg_complete <- tibble("time" = seq(as.Date("2005-1-01"), as.Date("2024-12-01"), "month")) %>% 
     mutate(
         time = format(time, "%Y-%m")
     )
-fg_final <- fg_complete %>% 
-    left_join(fg_uncomplete) %>% 
+fg_down <- fg_complete %>% 
+    left_join(fg_uncomplete[fg_uncomplete$type != "DI" & fg_uncomplete$dir == "D", ]) %>% 
     mutate(
         fg = replace_na(fg, 0)
     )
-#mam zde nekdy dve pozorovani pro jeden cas i IR i DI, jak to resit? prozatim jsem to DI proste vyhodil v tom ts()
+fg_up <- fg_complete %>% 
+    left_join(fg_uncomplete[fg_uncomplete$type != "DI" & fg_uncomplete$dir == "U", ]) %>% 
+    mutate(
+        fg = replace_na(fg, 0)
+    )
 
 #převod na ts objekty
-hcpi_ts <- ts(hcpi[[2]], start = c(2000, 1), frequency = 12)
-unemp_ts <- ts(unlist(unemp[unemp[[1]] == "Celkem ČR", ][-1]), start = c(2005, 1), frequency = 12)
 asset_ts <- ts(b_sheet[[13]][nrow(b_sheet):1], start = c(2002, 9), frequency = 12)
-ir_ts <- ts(ir[[2]][nrow(ir):1], start = c(1995, 12), frequency = 12)
-fg_ts <- ts(replace_na(unlist(fg_final[fg_final$type != "DI", 2]), 0), start = c(2005, 1), end = c(2024, 11), frequency = 12)
+fg_down_ts <- ts(fg_down[[2]], start = c(2005, 1), end = c(2024, 11), frequency = 12)
+fg_up_ts <- ts(fg_up[[2]], start = c(2005, 1), end = c(2024, 11), frequency = 12)
 ie_p_ts <- ts(ie_p[[2]], start = c(1999, 5), frequency = 12)
 ie_h_ts <- ts(ie_h[[2]], start = c(2001, 1), frequency = 12)
+ir_ts <- ts(ir[[2]][nrow(ir):1], start = c(1995, 12), frequency = 12)
+hcpi_ts <- ts(hcpi[[2]], start = c(2000, 1), frequency = 12)
+unemp_ts <- ts(unlist(unemp[unemp[[1]] == "Celkem ČR", ][-1]), start = c(2005, 1), frequency = 12)
 
 data <- tibble("datum" = format(seq(as.Date("2005-1-01"), as.Date("2023-12-01"), "month"), "%Y-%m"),
                "aktiva" = window(asset_ts, start = c(2005, 1), end = c(2023, 12)),
-               "forward_guidance" = window(fg_ts, start = c(2005, 1), end = c(2023, 12)), 
+               "forward_guidance_uvolneni" = window(fg_down_ts, start = c(2005, 1), end = c(2023, 12)),
+               "forward_guidance_zprisneni" = window(fg_up_ts, start = c(2005, 1), end = c(2023, 12)),
                "nezamestnanost" = window(unemp_ts, start = c(2005, 1), end = c(2023, 12)),
                "urok" = window(ir_ts, start = c(2005, 1), end = c(2023, 12)),
                "inflace" = window(hcpi_ts, start = c(2005, 1), end = c(2023, 12)),
@@ -50,26 +56,13 @@ data <- tibble("datum" = format(seq(as.Date("2005-1-01"), as.Date("2023-12-01"),
 #stacionarni
 adf.test(data$oce_p)
 adf.test(data$oce_h)
+adf.test(data$forward_guidance_uvolneni)
 #nestacionarni
 adf.test(data$inflace)
-adf.test(data$forward_guidacne)
+adf.test(data$forward_guidance_zprisneni)
 adf.test(data$urok)
 adf.test(data$aktiva)
 adf.test(data$nezamestnanost)
-
-#differenciace nestacionarnich
-
-data$inflace <- c(NA, diff(data$inflace))
-adf.test(na.remove(data$inflace))
-
-data$aktiva <- c(NA, diff(data$aktiva))
-adf.test(na.remove(data$aktiva))
-
-data$urok <- c(NA, diff(data$urok))
-adf.test(na.remove(data$urok))
-
-data$nezam <- c(NA, diff(data$nezamestnanost))
-adf.test(na.remove(data$nezam))
 
 #vyber zpozdeni
 lag_optimal <- VARselect(data[-1, ], lag.max = 10, type = "both")
