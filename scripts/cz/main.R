700406b6f4f8b37c98240493d7a6a0a6127c1126
Vašek Jež <164062935+eolybq@users.noreply.github.com>
2025-01-31 02:36:52 +0100

    Update main.R

--- scripts/cz/main.R
+++ scripts/cz/main.R
@@ -1,1 +1,12 @@
-added main
+# start by loading in raw data and getting it clean for analysis 
+do_clean_data <- TRUE
+# now run the main statistical model
+do_estimate <- TRUE
+# now plot the analysis
+do_plot <- TRUE
+
+if (do_clean_data) source("clean_data.R", echo = TRUE)
+
+if (do_estimate) source("estimate.R", echo = TRUE)
+
+if (do_plot) source("plot.R", echo = TRUE)
