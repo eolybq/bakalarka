3172ded481074006f87ee015b66b5b36813646c0
eolybq <jez.vasek@gmail.com>
2025-02-13 06:50:27 +0100

    clean up, hdp aktiva scaled, irf granger variance decomposition, differences of every nonstationary var

--- CZ/scripts/estimate.R
+++ CZ/scripts/estimate.R
@@ -10,87 +10,13 @@ library(vars)
 load("data/tibble_data.RData")
 
 
-# GRAF vsechny promenne ============
-load("data/ts_data.RData")
-
-# Create a list of all ts objects loaded from ts_data.RData
-ts_objects <- list(
-    cpi_ts = cpi_ts,
-    asset_ts = asset_ts,
-    fg_down_ts = fg_down_ts,
-    fg_up_ts = fg_up_ts,
-    ie_p_ts = ie_p_ts,
-    ie_h_ts = ie_h_ts,
-    ir_ts = ir_ts,
-    unemp_ts = unemp_ts
-)
-
-# HACK: mozna predelat jen na autoplot neb tak asi zbytecny
-ts_to_df <- function(ts_obj) {
-    start_year <- start(ts_obj)[1]
-    start_period <- start(ts_obj)[2]
-    freq <- frequency(ts_obj)
-    n <- length(ts_obj)
-
-    start_date <- as.Date(paste0(start_year, "-", sprintf("%02d", start_period), "-01"))
-    time_seq <- seq(from = start_date, by = "month", length.out = n)
-
-    tibble(time = time_seq, value = as.numeric(ts_obj))
-}
-
-
-# Loop over each time series, convert to a tibble, and generate a ggplot graph
-for (ts_name in names(ts_objects)) {
-    df <- ts_to_df(ts_objects[[ts_name]])
-    p <- ggplot(df, aes(x = time, y = value)) +
-        geom_line(color = "steelblue", linewidth = 1) +
-        labs(
-            title = paste("Time Series Plot:", ts_name),
-            x = "Time",
-            y = "Value"
-        ) +
-        theme_minimal()
-
-    print(p)
-}
-
-
-# ACF - stacionarita | PACF - sezonnost ===========
-acf_pacf <- function(data, maxlag) {
-    if (class(data) %in% c("numeric", "ts")) {
-        acf(as.numeric(data), lag.max = maxlag, main = paste("ACF for", deparse(substitute(data))))
-        pacf(as.numeric(data), lag.max = maxlag, main = paste("PACF for", deparse(substitute(data))))
-    } else if (class(data) %in% c("tibble", "data.frame")) {
-        imap(data, function(x, y) {
-            # par(mfrow = c(1, 2))
-
-            acf(as.numeric(x), lag.max = maxlag, main = paste("ACF for", y))
-            pacf(as.numeric(x), lag.max = maxlag, main = paste("PACF for", y))
-        })
-    }
-}
-
-# FIX: unemp_ts vypadá sezonne - resit
-acf_pacf(ts_objects[["unemp_ts"]], 37)
-
-
-sdiffed_unemp <- diff(ts_objects[["unemp_ts"]], lag = 12)
-autoplot(sdiffed_unemp)
-acf_pacf(sdiffed_unemp, 37)
-
-s2diffed_unemp <- diff(sdiffed_unemp, lag = 13)
-autoplot(s2diffed_unemp)
-acf_pacf(s2diffed_unemp, 37)
-
-check_stationarity_summary(ts_objects["unemp_ts"])
-
-
-# předpoklady
-# TODO: dodělat i klasické předpoklady pro rezidua
+# ========================================================
+# Data
+# ========================================================
 
 # NOTE: Funkce na stacionaritu a kointegraci z manahra_pred
 # Stacionarita a tabulka p hodnot testu a pocet diferenci k dosahnuti stacionarity
-check_stationarity_summary <- function(data) {
+check_stationarity <- function(data) {
     results <- tibble(
         Variable = character(),
         KPSS_p_value = numeric(),
@@ -118,36 +44,263 @@ check_stationarity_summary <- function(data) {
     return(results)
 }
 
-variables <- drop_na(tibble_data[, c("aktiva", "forward_guidance_uvolneni", "forward_guidance_zprisneni", "oce_h", "oce_p", "inflace", "urok", "nezamestnanost")])
-check_stationarity_summary(variables)
+variables <- drop_na(tibble_data[, -1])
+check_stationarity(variables)
 
-tibble_data <- tibble_data |>
+
+# ACF - stacionarita | PACF - sezonnost ===========
+acf_pacf <- function(data, maxlag) {
+    if (class(data)[1] %in% c("numeric", "ts")) {
+        acf(as.numeric(data), lag.max = maxlag, main = paste("ACF for", deparse(substitute(data))))
+        pacf(as.numeric(data), lag.max = maxlag, main = paste("PACF for", deparse(substitute(data))))
+    } else if (class(data)[1] %in% c("tbl_df", "tbl", "data.frame")) {
+        imap(data[, -1], function(x, y) {
+            # par(mfrow = c(1, 2))
+
+            acf(as.numeric(x), lag.max = maxlag, main = paste("ACF for", y))
+            pacf(as.numeric(x), lag.max = maxlag, main = paste("PACF for", y))
+        })
+    }
+}
+
+acf_pacf(tibble_data, 40)
+
+
+diffed_unemp <- diff(tibble_data[["nezam"]], lag = 1)
+autoplot(diffed_unemp)
+acf_pacf(diffed_unemp, 37)
+
+# NOTE: Je toto potřeba?
+s2diffed_unemp <- diff(diffed_unemp, lag = 12)
+autoplot(s2diffed_unemp)
+acf_pacf(s2diffed_unemp, 37)
+
+check_stationarity(tibble(first_diff = diffed_unemp))
+check_stationarity(tibble(s_diff = s2diffed_unemp))
+
+
+# TODO: Johansen cointegration test
+
+
+
+trans_tdata <- tibble_data |>
     mutate(
-        aktiva = log(aktiva),
+        aktiva = c(NA, diff(log(aktiva), lag = 1)),
+        aktiva_scaled = c(NA, diff(aktiva_scaled, lag = 1)),
+        nezam = c(NA, diff(nezam, lag = 1)),
+        urok = c(NA, NA, diff(diff(urok, lag = 1))),
+        inflace = c(NA, NA, diff(diff(inflace, lag = 1))),
+        oce_p = c(NA, diff(oce_p, lag = 1)),
+        oce_h = c(NA, diff(oce_h, lag = 1))
+    ) |>
+    dplyr::select(
+        -datum,
+    ) |>
+    drop_na()
+
+variables <- trans_tdata
+check_stationarity(variables)
+
+
+
+# ========================================================
+# Model
+# ========================================================
+
+# DOMACNOSTI ==============
+trans_tdata_h <- trans_tdata |>
+    dplyr::select(
+        -oce_p,
+        -forward_guidance_uvolneni,
+        -forward_guidance_zprisneni,
+
+        # NOTE: swap za aktiva kdyztak
+        -aktiva
     )
 
-
-
-# Johansen cointegration test
-
-
-
-
-
-
-
-
 # vyber zpozdeni
-lag_optimal <- VARselect(tibble_data[, -1], lag.max = 10, type = "both")
+lag_optimal <- VARselect(
+    trans_tdata_h,
+    lag.max = 20,
+    type = "const",
+    exogen = tibble(
+        fg_u = trans_tdata$forward_guidance_uvolneni,
+        fg_z = trans_tdata$forward_guidance_zprisneni
+    )
+)
 lag_optimal
 
-# model
-var_model_prof <- VAR(tibble_data[, -c(1, 3, 4, ncol(tibble_data))], p = 10, exogen = tibble(tibble_data$forward_guidance_uvolneni, tibble_data$forward_guidance_zprisneni)) # TODO: type: Type of deterministic regressors to include.
-summary(var_model_prof)
+# odhad
+var_model_domac <- trans_tdata_h |>
+    VAR(
+        p = 13,
+        type = "const",
+        exogen = tibble(
+            fg_u = trans_tdata$forward_guidance_uvolneni,
+            fg_z = trans_tdata$forward_guidance_zprisneni
+        )
+    )
+
+summary(var_model_domac)
+
+
+# prepoklady
+# TODO: dodelat asi klasicke predpoklady jak u klasicke regrese
+
 
 # diagnostika modelu
-residua <- residuals(var_model)
-adf.test(residua) # stacionarity residui
-serial.test(var_model) # autokorelace residui
-acf(residuals)
-pacf(residuals)
+residuals <- residuals(var_model_domac)
+
+serial.test(var_model_domac)
+arch.test(var_model_domac)
+normality.test(var_model_domac)
+
+# KPSS test
+kpss_results <- apply(residuals, 2, kpss.test)
+# ADF test
+adf_results <- apply(residuals, 2, adf.test)
+# PP test
+pp_results <- apply(residuals, 2, pp.test)
+# Print the results
+print(kpss_results)
+print(adf_results)
+print(pp_results)
+
+
+# IRF
+# FIX: impulse musi byt endogenni??
+# oce_h_fg_z_irf <- irf(var_model_domac,
+#     impulse = "fg_z", response = "oce_h",
+#     n.ahead = 20, ortho = FALSE
+# )
+#
+# plot(oce_h_fg_z_irf)
+
+oce_h_akt_irf <- irf(
+    var_model_domac,
+
+    # NOTE: swap za aktiva kdyztak
+    impulse = "aktiva_scaled",
+
+    response = "oce_h",
+    n.ahead = 20,
+    ortho = FALSE,
+    runs = 100
+)
+
+plot(oce_h_akt_irf)
+
+
+# Granger causality
+# NOTE: swap za aktiva kdyztak
+causality(var_model_domac, cause = "aktiva_scaled")
+
+# FIX: zase hazi chybu asi - exogenni
+# causality(var_model_domac, cause = "fg_z")
+# causality(var_model_domac, cause = "fg_u")
+
+
+# Variancni dekompozice - jak moc je promenna ovlivnena sokem ostatnich promennych
+v_decomp <- fevd(var_model_domac, n.ahead = 20)
+plot(v_decomp)
+
+
+
+
+
+# PROFESIONALOVE ==============
+trans_tdata_p <- trans_tdata |>
+    dplyr::select(
+        -oce_h,
+        -forward_guidance_uvolneni,
+        -forward_guidance_zprisneni,
+
+        # NOTE: swap za aktiva kdyztak
+        -aktiva
+    )
+
+# vyber zpozdeni
+lag_optimal <- VARselect(
+    trans_tdata_p,
+    lag.max = 20,
+    type = "const",
+    exogen = tibble(
+        fg_u = trans_tdata$forward_guidance_uvolneni,
+        fg_z = trans_tdata$forward_guidance_zprisneni
+    )
+)
+lag_optimal
+
+# odhad
+var_model_prof <- trans_tdata_p |>
+    VAR(
+        p = 14,
+        type = "const",
+        exogen = tibble(
+            fg_u = trans_tdata$forward_guidance_uvolneni,
+            fg_z = trans_tdata$forward_guidance_zprisneni
+        )
+    )
+
+summary(var_model_prof)
+
+
+# prepoklady
+# TODO: dodelat asi klasicke predpoklady jak u klasicke regrese
+
+
+# diagnostika modelu
+residuals <- residuals(var_model_prof)
+
+serial.test(var_model_prof)
+arch.test(var_model_prof)
+normality.test(var_model_prof)
+
+# KPSS test
+kpss_results <- apply(residuals, 2, kpss.test)
+# ADF test
+adf_results <- apply(residuals, 2, adf.test)
+# PP test
+pp_results <- apply(residuals, 2, pp.test)
+# Print the results
+print(kpss_results)
+print(adf_results)
+print(pp_results)
+
+
+# IRF
+# FIX: impulse musi byt endogenni??
+# oce_p_fg_z_irf <- irf(var_model_prof,
+#     impulse = "fg_z", response = "oce_p",
+#     n.ahead = 20, ortho = FALSE
+# )
+#
+# plot(oce_p_fg_z_irf)
+
+oce_p_akt_irf <- irf(
+    var_model_prof,
+
+    # NOTE: swap za aktiva kdyztak
+    impulse = "aktiva_scaled",
+
+    response = "oce_p",
+    n.ahead = 20,
+    ortho = FALSE,
+    runs = 100
+)
+
+plot(oce_p_akt_irf)
+
+
+# Granger causality
+# NOTE: swap za aktiva kdyztak
+causality(var_model_domac, cause = "aktiva_scaled")
+
+# FIX: zase hazi chybu asi - exogenni
+# causality(var_model_prof, cause = "fg_z")
+# causality(var_model_prof, cause = "fg_u")
+
+
+# Variancni dekompozice - jak moc je promenna ovlivnena sokem ostatnich promennych
+v_decomp <- fevd(var_model_prof, n.ahead = 20)
+plot(v_decomp)
--- CZ/scripts/preprocess_data.R
+++ CZ/scripts/preprocess_data.R
@@ -12,23 +12,24 @@ ir <- read_delim("data/rawdata/repo_prumer_m.csv", delim = ";")
 fg_uncomplete <- read_excel("data/rawdata/fg.xlsx")
 ie_p <- read_excel("data/rawdata/CZ_p_m.xlsx")
 ie_h <- read_excel("data/rawdata/CZ_h_m.xlsx")
+hdp <- read_delim("data/rawdata/hdp.csv", delim = ";")
 
 # doplnění implicitních chybějících hodnot ve FG a rozdeleni na uvoneni a zprisneni
 fg_uncomplete$time <- format(fg_uncomplete$time, "%Y-%m")
 fg_complete <- tibble("time" = seq(as.Date("2005-1-01"), as.Date("2024-12-01"), "month")) |>
-  mutate(
-    time = format(time, "%Y-%m")
-  )
+    mutate(
+        time = format(time, "%Y-%m")
+    )
 fg_down <- fg_complete |>
-  left_join(fg_uncomplete[fg_uncomplete$type != "DI" & fg_uncomplete$dir == "D", ]) |>
-  mutate(
-    fg = replace_na(fg, 0)
-  )
+    left_join(fg_uncomplete[fg_uncomplete$type != "DI" & fg_uncomplete$dir == "D", ]) |>
+    mutate(
+        fg = replace_na(fg, 0)
+    )
 fg_up <- fg_complete |>
-  left_join(fg_uncomplete[fg_uncomplete$type != "DI" & fg_uncomplete$dir == "U", ]) |>
-  mutate(
-    fg = replace_na(fg, 0)
-  )
+    left_join(fg_uncomplete[fg_uncomplete$type != "DI" & fg_uncomplete$dir == "U", ]) |>
+    mutate(
+        fg = replace_na(fg, 0)
+    )
 
 # převod na ts objekty
 
@@ -45,34 +46,47 @@ ie_p_ts <- ts(ie_p[[2]], start = c(1999, 5), frequency = 12)
 ie_h_ts <- ts(ie_h[[2]], start = c(2001, 1), frequency = 12)
 ir_ts <- ts(ir[[2]][nrow(ir):1], start = c(1995, 12), frequency = 12) # nolint: seq_linter.
 unemp_ts <- ts(unlist(unemp[unemp[[1]] == "Celkem ČR", ][-1]), start = c(2005, 1), frequency = 12)
+hdp_ts <- ts(arrange(hdp, Období)[[2]], start = c(1995, 1), frequency = 4)
+
+# Skalovana aktiva
+hdp_month <- hdp_ts |>
+    window(start = c(2005, 1), end = c(2023, 4)) |>
+    rep(each = 3)
+scl_aktiva <- window(asset_ts, start = c(2005, 1), end = c(2023, 12)) / hdp_month
+
 
 
 tibble_data <- tibble(
-  "datum" = format(seq(as.Date("2005-1-01"), as.Date("2023-12-01"), "month"), "%Y-%m"),
-  "aktiva" = (window(asset_ts, start = c(2005, 1), end = c(2023, 12))),
-  "forward_guidance_uvolneni" = window(fg_down_ts, start = c(2005, 1), end = c(2023, 12)),
-  "forward_guidance_zprisneni" = window(fg_up_ts, start = c(2005, 1), end = c(2023, 12)),
-  "nezamestnanost" = window(unemp_ts, start = c(2005, 1), end = c(2023, 12)),
-  "urok" = window(ir_ts, start = c(2005, 1), end = c(2023, 12)),
-  "inflace" = window(cpi_ts, start = c(2005, 1), end = c(2023, 12)),
-  "oce_p" = window(ie_p_ts, start = c(2005, 1), end = c(2023, 12)),
-  "oce_h" = window(ie_h_ts, start = c(2005, 1), end = c(2023, 12))
+    "datum" = format(seq(as.Date("2005-1-01"), as.Date("2023-12-01"), "month"), "%Y-%m"),
+    "aktiva" = window(asset_ts, start = c(2005, 1), end = c(2023, 12)),
+    "aktiva_scaled" = scl_aktiva,
+    "forward_guidance_uvolneni" = window(fg_down_ts, start = c(2005, 1), end = c(2023, 12)),
+    "forward_guidance_zprisneni" = window(fg_up_ts, start = c(2005, 1), end = c(2023, 12)),
+    "nezam" = window(unemp_ts, start = c(2005, 1), end = c(2023, 12)),
+    "urok" = window(ir_ts, start = c(2005, 1), end = c(2023, 12)),
+    "inflace" = window(cpi_ts, start = c(2005, 1), end = c(2023, 12)),
+    "oce_p" = window(ie_p_ts, start = c(2005, 1), end = c(2023, 12)),
+    "oce_h" = window(ie_h_ts, start = c(2005, 1), end = c(2023, 12))
+)
+
+ts_objects <- list(
+    cpi_ts = cpi_ts,
+    asset_ts = asset_ts,
+    fg_down_ts = fg_down_ts,
+    fg_up_ts = fg_up_ts,
+    ie_p_ts = ie_p_ts,
+    ie_h_ts = ie_h_ts,
+    ir_ts = ir_ts,
+    unemp_ts = unemp_ts
 )
 
 
 save(
-  cpi_ts,
-  asset_ts,
-  fg_down_ts,
-  fg_up_ts,
-  ie_p_ts,
-  ie_h_ts,
-  ir_ts,
-  unemp_ts,
-  file = "data/ts_data.RData"
+    ts_objects,
+    file = "data/ts_data.RData"
 )
 
 save(
-  tibble_data,
-  file = "data/tibble_data.RData"
+    tibble_data,
+    file = "data/tibble_data.RData"
 )
